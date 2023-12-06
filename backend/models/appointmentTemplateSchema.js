6f59abf76722130f2b4f6eb591eb09bfdd4cf757
Your Name <kristian.ivanov.pr@gmail.com>
2023-12-06 11:56:42 +0200

    added template for days and hours

---
+++ backend/models/appointmentTemplateSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const appointmentTemplateSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    required: true,
+  },
+  timeSlots: {
+    type: [String],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('AppointmentTemplate', appointmentTemplateSchema);
