e108130f8e2a8073cbeda9530ea7654823bd1ae3
Your Name <kristian.ivanov.pr@gmail.com>
2023-12-07 11:26:14 +0200

    made functions to appoint hour and to cancel appointment

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const AppointmentTemplate = require("../models/appointmentTemplateSchema");
+const UserAppointment = require("../models/userAppointmentHour");
 
 //function for creating a template day. 
 const createTemplate = async (req, res) => {
@@ -20,7 +21,64 @@ const getHours = async (req, res) => {
     res.status(200).json(hours);
 }
 
+const getHoursForUser = async (req, res) => {
+    const {user_id} = req.body;
+    const appointment = await UserAppointment.find({user_id})
+    res.status(400).json({appointment});
+}
+
+const createAppointment = async (req, res) => {
+    const {day, hour, user_id} = req.body;
+
+    let emptyFields = [];
+    if(!day){
+        emptyFields.push("day")
+    }
+    if(!hour){
+        emptyFields.push("hour")
+    }
+    if(emptyFields.length > 0){
+        return res.status(400).json({error: "Please fill in all the fields", emptyFields});
+    }
+    const searchIfFree = await AppointmentTemplate.find({day});
+    //checks to see if there is an available slot on that day
+    if(!searchIfFree[0].timeSlots.includes(hour)){
+        return res.status(400).json({error: "This hour is already appointed"});;
+    }
+    try {
+        const hours = searchIfFree[0].timeSlots;
+        const filteredArray = hours.filter(item => item !== hour);
+        const reworkedTemplate = await AppointmentTemplate.findOneAndUpdate({day},{$set: {timeSlots: filteredArray}})
+        const appointedHour = await UserAppointment.create({user_id, day, hour})
+        
+        res.status(200).json(appointedHour);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
+
+const deleteAppointment = async (req,res) => {
+    const {_id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)){
+        return res.status(404).json({error: "No such appointed hour"})
+    }
+    const appointedHour = await UserAppointment.findOneAndDelete({_id});
+    if (!appointedHour) {
+        return res.status(404).json({error: "No such appointed hour"});
+    }
+    const dayOfWeek = await AppointmentTemplate.find({day: appointedHour.day});
+    const array = dayOfWeek[0].timeSlots;
+    array.push(appointedHour.hour);
+    const reworkedTemplate = await AppointmentTemplate.findOneAndUpdate({day: appointedHour.day},{$set: {timeSlots: array}})
+    res.status(200).json({mssg: "Sucessfully deleted"});
+
+}
+
 module.exports = {
     createTemplate,
-    getHours
+    getHours,
+    getHoursForUser,
+    createAppointment,
+    deleteAppointment
 }
--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {createTemplate, getHours} = require("../controllers/appointmentController")
+const {createTemplate, getHours, getHoursForUser, createAppointment, deleteAppointment} = require("../controllers/appointmentController")
 
 //router for appointments
 const router = express.Router();
@@ -7,5 +7,11 @@ const router = express.Router();
 router.post("/template", createTemplate);
 
 router.get("/", getHours);
+
+router.get("/userAppointment", getHoursForUser);
+
+router.post("/", createAppointment)
+
+router.delete("/:_id", deleteAppointment)
 
 module.exports = router;
