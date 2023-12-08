2554f012e16f48ee1b31fda10da3d9482d02eeab
Your Name <kristian.ivanov.pr@gmail.com>
2023-12-08 10:27:49 +0200

    created basic navbar and set color variables in css

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,23 @@
+const NavBar = () => {
+    return (
+        <header>
+            <div>
+                <h2>Revive & Shine <span>Detailing</span></h2>
+            </div>
+            <nav>
+                <ul>
+                    <li>Services</li>
+                    <li>About us</li>
+                    <li>Contacts</li>
+                    <li>Appoint hour</li>
+                </ul>
+            </nav>
+            <div>
+                <button>Sign Up</button>
+                <button>Log In</button>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar;
