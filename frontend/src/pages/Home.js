46123b5f3d0ec1f5f52213d0a7db77c1cd1cdd78
Your Name <kristian.ivanov.pr@gmail.com>
2023-12-10 16:40:59 +0200

    added new component for reasons why people should choose us

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,22 @@
 import {Link} from "react-router-dom"
+import WhyChooseUs from "../components/WhyChooseUs"
 
 const Home = () => {
     return (
       <div>
-        <section className="hero-section">
-          <div className="hero-content">
-            <h1>Welcome to Revive & Shine Detailing</h1>
-            <p>Your destination for top-notch car detailing services. We bring your vehicle to life with our expert detailing techniques, cutting-edge products, and a passion for perfection.</p>
-            <p>Experience the ultimate in automotive care and let your vehicle shine like never before. Whether it's a thorough exterior cleaning, interior rejuvenation, or a complete detailing package, we've got you covered.</p>
-            <Link to="/services">
-              <button className="btn">Explore Our Services</button>
-            </Link>
-          </div>
-        </section>
+        <div className="hero-container">
+          <section className="hero-section">
+            <div className="hero-content">
+              <h1>Welcome to Revive & Shine Detailing</h1>
+              <p>Your destination for top-notch car detailing services. We bring your vehicle to life with our expert detailing techniques, cutting-edge products, and a passion for perfection.</p>
+              <p>Experience the ultimate in automotive care and let your vehicle shine like never before. Whether it's a thorough exterior cleaning, interior rejuvenation, or a complete detailing package, we've got you covered.</p>
+              <Link to="/services">
+                <button className="btn">Explore Our Services</button>
+              </Link>
+            </div>
+          </section>
+          <WhyChooseUs/>
+        </div>
       </div>
     )
 }
