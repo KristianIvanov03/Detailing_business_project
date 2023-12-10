49502cb3823045b867ff399f6ba6a13824b85f2a
Your Name <kristian.ivanov.pr@gmail.com>
2023-12-10 16:25:52 +0200

    installed and set up font awesome

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTools, faCubes, faSmile } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faTools, faCubes, faSmile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
