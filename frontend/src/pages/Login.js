323863e1620c36e6887338c4bdb5069328941d51
Your Name <kristian.ivanov.pr@gmail.com>
2024-01-16 11:23:26 +0200

    login functionality added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,19 +1,39 @@
 // Login.jsx
-import React from 'react';
+import React, {useState} from 'react';
+import {useLogin} from '../hooks/useLogin'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const {login, isLoading, error} = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(email, password)
+  }
   return (
     <main>
         <div className="login-page">
             <h2>Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" />
+                <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}/>
 
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" />
+                <input 
+                type="password" 
+                id="password" 
+                name="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password} />
 
-                <button type="submit" className='btn'>Log In</button>
+                <button type="submit" className='btn' disabled={isLoading}>Log In</button>
+                {error&&<div className="error">{error}</div>}
             </form>
         </div>
     </main>
