b9337588bbb3582557144b10b9a752931ee3f716
Your Name <kristian.ivanov.pr@gmail.com>
2024-01-17 10:53:27 +0200

    provider changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTools, faCubes, faSmile } from '@fortawesome/free-solid-svg-icons';
 import {AuthContextProvider} from './context/AuthContext'
+import {HoursTemplateContextProvider} from './context/HoursTemplateContext'
 
 library.add(faTools, faCubes, faSmile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <HoursTemplateContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </HoursTemplateContextProvider>
   </React.StrictMode>
 );
 
